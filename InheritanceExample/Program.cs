dda2b7f4ad75be7df9cac70430a33aa09726f9fa
mmeetk2314 <mk060@atcvisioncollege.ac.nz>
2021-05-21 14:32:18 +1200

    Add project files.

---
+++ InheritanceExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+    class Program
+    {
+        // Base Class
+
+        public class Vehicle
+        {
+            public Vehicle()
+            {
+                Console.WriteLine(" I am in the Constructor Vehicle");
+            }
+        }
+
+        // Derived Class
+        public class Truck : Vehicle
+        {
+            public Truck()
+            {
+                Console.WriteLine("I am in the Derived Truck");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
